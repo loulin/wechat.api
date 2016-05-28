5fc13d6ba0ea8f6692a7541fed3d0b071c32cab4
loulin <lin.lou@hotmail.com>
2016-05-28 10:50:15 +0800

    add logger option

--- README.md
+++ README.md
@@ -7,10 +7,18 @@ Wechat API
 3. Call api method with api path and request parameters. For api(path, data), if request method is get, data should be querystring or not set, otherwise it should be post object.
 
 ```js
-const Wechat = require('./');
+const Wechat = require('wechat.api');
 const wechat = new Wechat({
   appid: 'xxx',
   secret: 'xxx',
+  logger(options, result) {
+    console.log(options.uri);
+    if (result instanceof Error) {
+      console.error(result.message);
+    } else {
+      console.log(result);
+    }
+  },
 });
 
 wechat.api('/cgi-bin/user/info', 'openid=xxx&lang=en').then(result => {
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const debug = require('debug')('wechat:api');
+const TransformError = require('request-promise/errors').TransformError;
 const rp = require('request-promise').defaults({
   json: true,
   transform(body) {
@@ -18,6 +19,7 @@ class Wechat {
     appid,
     secret,
     defaultOptions,
+    logger,
     getToken = () => Promise.resolve(this.token),
     setToken = (token) => {
       this.token = token;
@@ -27,20 +29,21 @@ class Wechat {
     this.appid = appid;
     this.secret = secret;
     this.defaultOptions = defaultOptions;
+    this.logger = logger;
     this.getToken = getToken;
     this.setToken = setToken;
     this.origin = 'https://api.weixin.qq.com';
     this.token = {};
   }
 
-  api(entry, data, rpOptions, reload) {
+  api(entry, data, rpOptions, retry) {
     const options = Object.assign({}, this.defaultOptions, {
       method: 'GET',
       uri: this.origin + entry,
     }, rpOptions);
 
     return this.getToken().then(token => {
-      if (reload || !token || !token.access || token.expire > Date.now()) {
+      if (retry || !token || !token.access || token.expire > Date.now()) {
         return this.getAccessToken();
       }
 
@@ -56,23 +59,40 @@ class Wechat {
       }
 
       debug(`${options.method} ${options.uri}`);
-      return rp(options).then(result => result).catch(error => {
-        if (!reload && error.cause && error.cause.code === 40001) {
+      return this.request(options).catch(error => {
+        if (!retry && error instanceof TransformError && error.cause.code === 40001) {
           debug(`${error.message}, retry...`);
           return this.api(entry, data, rpOptions, true);
         }
 
         return Promise.reject(error);
       });
-    }).catch(error => Promise.reject(error.cause || error));
+    });
+  }
+
+  request(options) {
+    if (typeof this.logger === 'function') {
+      return rp(options).then(body => {
+        this.logger(options, body);
+        return body;
+      }).catch(error => {
+        this.logger(options, error);
+        return Promise.reject(error);
+      });
+    }
+
+    return rp(options);
   }
 
   getAccessToken() {
-    const url = `${this.origin}/cgi-bin/token?grant_type=client_credential` +
+    const uri = `${this.origin}/cgi-bin/token?grant_type=client_credential` +
       `&appid=${this.appid}&secret=${this.secret}`;
 
-    debug(`GET ${url}`);
-    return rp.get(url).then(result => {
+    debug(`GET ${uri}`);
+    return this.request({
+      method: 'GET',
+      uri,
+    }).then(result => {
       this.token.access = result.access_token;
       this.token.expire = Date.now() + (result.expires_in - 10) * 1000;
       this.setToken(this.token);
